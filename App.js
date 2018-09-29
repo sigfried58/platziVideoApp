1e8912308daac0efc552c873b138aaabf433ffb8
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-09-29 16:45:33 -0300

    Installing react-native-video

--- App.js
+++ App.js
@@ -6,6 +6,7 @@
 
 import React, { Component, Fragment } from 'react';
 import { Text, View } from 'react-native';
+import Video from 'react-native-video';
 import Header from './src/screens/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
 import CategoryList from './src/videos/containers/category-list';
@@ -34,6 +35,22 @@ export default class App extends Component<Props> {
     return (
       <Home>
         <Header />
+        <View style={{ flex: 1, height: 100 }}>
+          <Video
+            source={{
+              uri:
+                'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'
+            }}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0
+            }}
+            resizeMode="contain"
+          />
+        </View>
         <Text>buscador</Text>
         <Text>categoria</Text>
         {this.state.loading ? (
